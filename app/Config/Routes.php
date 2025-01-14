b8d80615a3ca115d3a33748e6d400fe1ce79b07e
FRChanned <mathisperot420@gmail.com>
2025-01-14 14:52:41 +0100

    deb pages stat

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+ $routes->get('/', 'Home::index', ['as' => 'index']);
+ 
+ $routes->get('/playstem', 'PlayStem::index', ['as' => 'playstem']);
--- app/Controllers/PlayStem.php
+++ app/Controllers/PlayStem.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class PlayStem extends BaseController
 {
     public function index(): string
     {
