84896445fc131c62dd931bdf814b19b503182d52
Tueyss82 <khantouchabderrahil57@gmail.com>
2025-01-17 16:26:14 +0100

    co18

--- app/Views/PlayStem/Exercice.php
+++ app/Views/PlayStem/Exercice.php
@@ -2,7 +2,17 @@
 <?= $this->section('contenu') ?>
 
 
-<div class="supportJeton"><img src="image\supportJeton.png"></div>    
+<div class="exercices-TextLayout">
+    <p>Exercices</p>
+</div>
 
-<div class="grilleRobot"><img src="image\grilleRobot.png" ></div>   
+<div class="consignes"><img src="image\consignes.png"></div>
+<div class="image-container ">
+    <div class="supportJeton"><img src="image\supportJeton.png"></div>
+    <div class="fleche-haut"><img src="image\fleche-haut.png"></div>
+    <div class="grilleRobot"><img src="image\grilleRobot.png"></div>
+</div>
+
+<div class="telecommande"><img src="image\telecommande.png"></div>
+
 <?= $this->endSection() ?>
