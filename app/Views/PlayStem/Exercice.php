f7069d9a6ead2fcb376c183fdf65fdda89351c36
Tueyss82 <khantouchabderrahil57@gmail.com>
2025-01-17 16:43:38 +0100

    co18

--- app/Views/PlayStem/Exercice.php
+++ app/Views/PlayStem/Exercice.php
@@ -8,11 +8,21 @@
 
 <div class="consignes"><img src="image\consignes.png"></div>
 <div class="image-container ">
+
     <div class="supportJeton"><img src="image\supportJeton.png"></div>
-    <div class="fleche-haut"><img src="image\fleche-haut.png"></div>
+    <div class="fleche-direction" id="direction"> <img src="image\fleche-haut.png"></div>
+    
     <div class="grilleRobot"><img src="image\grilleRobot.png"></div>
 </div>
 
 <div class="telecommande"><img src="image\telecommande.png"></div>
 
+<script>
+        direction.onclick = function() {
+            if (direction.onclick){
+                direction.inner.Html = `<div class = "fleche-haut" <img ` 
+            }
+        }
+    </script>
+    
 <?= $this->endSection() ?>
