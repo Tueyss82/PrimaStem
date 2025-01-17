d703d17fdfe60def5e341e19e973806c24f415ca
Tueyss82 <khantouchabderrahil57@gmail.com>
2025-01-17 11:54:57 +0100

    cxd

--- app/Views/PlayStem/Exercice.php
+++ app/Views/PlayStem/Exercice.php
@@ -2,6 +2,7 @@
 <?= $this->section('contenu') ?>
 
 
+<div class="supportJeton"><img src="image\supportJeton.png"></div>    
 
-
+<div class="grilleRobot"><img src="image\grilleRobot.png" ></div>   
 <?= $this->endSection() ?>
--- app/Views/PlayStem/layoutPlayStem.php
+++ app/Views/PlayStem/layoutPlayStem.php
@@ -51,10 +51,10 @@
 </header>
 
 <body>
-    <?= $this->renderSection('contenu') ?>
+<?= $this->renderSection('contenu') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-<footer>
+    
+    <footer>
     <div class="footer">
         <div class="footerText">
             &copy; <?= date('Y') ?> PrimaSTEM & co.
@@ -73,8 +73,9 @@
                     Instagram : PrimaStem
                     </a>
                 </div>
-                <p></p>
             </div>
         </div>
     </div>
 </footer>
+    
+</body>
