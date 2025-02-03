aa8eec3aab7cf192966491145b46167e07a6c220
FRChanned <mathisperot420@gmail.com>
2025-02-03 10:59:13 +0100

    ajout balises pour le google analytics

--- app/Views/layoutPrimaStem.php
+++ app/Views/layoutPrimaStem.php
@@ -1,6 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-MXLRLT6B');
+    </script>
+    <!-- End Google Tag Manager -->
+</head>
+
+<?= $this->renderSection('head') ?>
+
+
 
 <!-- HEADER: MENU + HEROE SECTION -->
 <header>
@@ -35,8 +59,8 @@
                         <a class="nav-link" href="<?= url_to('playstem') ?>">PlayStem</a>
                     </li>
                     <?php
-                        $user = auth()->user();
-                        $admin = $user && $user->inGroup('admin');
+                    $user = auth()->user();
+                    $admin = $user && $user->inGroup('admin');
                     ?>
                     <?= $admin ? '<li class="nav-item">
                         <a class="nav-link" href="' . url_to('logout') . '">Se Déconnecter</a>
@@ -53,6 +77,10 @@
 </header>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXLRLT6B"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?= $this->renderSection('contenu') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
