eadb76d218a6d71244c1019473cd4ffa6498363b
Tueyss82 <khantouchabderrahil57@gmail.com>
2025-02-07 17:58:41 +0100

    co07

--- app/Views/PlayStem/Exercice.php
+++ app/Views/PlayStem/Exercice.php
@@ -62,7 +62,7 @@
     .character {
         width: 50px;
         height: 50px;
-        
+
         position: absolute;
         top: 50%;
         left: 50%;
@@ -76,30 +76,33 @@
         display: none;
         /* Masqué par défaut */
     }
-    
+
+    .boutons {
+        display: flex
+    }
 </style>
 </head>
 
 <body>
 
     <div class="sidebar">
-
-        <div class="mouvement" id="mouvement">
-            <button ><img src="imagesPlayStem\mouvementCategorie.png"></button>
+        <div class="boutons">
+            <div class="mouvement" id="mouvement">
+                <button><img src="imagesPlayStem\mouvementCategorie.png"></button>
+            </div>
+            <div class="rotation" id="rotation">
+                <button><img src="imagesPlayStem\repetitionCategorie.png"></button>
+            </div>
+            <div class="nombres" id="nombres">
+                <button><img src="imagesPlayStem\nombresCategorie.png"></button>
+            </div>
+            <div class="operations" id="operations">
+                <button><img src="imagesPlayStem\operationsCategorie.png"></button>
+            </div>
+            <div class="speciaux" id="fonction">
+                <button><img src="imagesPlayStem\speciauxCategorie.png"></button>
+            </div>
         </div>
-        <div class="rotation" id="rotation">
-            <button><img src="imagesPlayStem\repetitionCategorie.png"></button>
-        </div>
-        <div class="nombres" id="nombres">
-            <button><img src="imagesPlayStem\nombresCategorie.png"></button>
-        </div>
-        <div class="operations" id="operations">
-            <button><img src="imagesPlayStem\operationsCategorie.png"></button>
-        </div>
-        <div class="speciaux" id="fonction">
-            <button><img src="imagesPlayStem\speciauxCategorie.png"></button>
-        </div>
-
         <div class="draggable" id="jeton1"> </div>
         <div class="draggable" id="jeton2"> </div>
         <div class="draggable" id="jeton3"> </div>
@@ -195,34 +198,34 @@
         <!-- <div class="supportJeton"> <img src="../imagesPlayStem/supportJeton.png" class="imageSupport"> -->
         <div class="storage" id="storage-box">
             <img src="../imagesPlayStem/telecommande.png">
-            <div class="drop-circle" style="margin-top: 10px; left: 43px;"></div>
-            <div class="drop-circle" style="margin-top: 10px; left: 124px;"></div>
-            <div class="drop-circle" style="margin-top: 10px; left: 201px;"></div>
-            <div class="drop-circle" style="margin-top: 10px; left: 280px;"></div>
-            <div class="drop-circle" style="margin-top: 10px; left: 350px;"></div>
-            <div class="drop-circle" style="margin-top: 10px; left: 421px;"></div>
+            <div class="drop-circle" id="circle1" style="margin-top: 10px; left: 43px;"></div>
+            <div class="drop-circle" id="circle2" style="margin-top: 10px; left: 124px;"></div>
+            <div class="drop-circle" id="circle3" style="margin-top: 10px; left: 201px;"></div>
+            <div class="drop-circle" id="circle4" style="margin-top: 10px; left: 280px;"></div>
+            <div class="drop-circle" id="circle5" style="margin-top: 10px; left: 350px;"></div>
+            <div class="drop-circle" id="circle6" style="margin-top: 10px; left: 421px;"></div>
 
-            <div class="drop-circle" style="margin-top: 76px; left: 43px;"></div>
-            <div class="drop-circle" style="margin-top: 76px; left: 124px;"></div>
-            <div class="drop-circle" style="margin-top: 76px; left: 201px;"></div>
-            <div class="drop-circle" style="margin-top: 76px; left: 280px;"></div>
-            <div class="drop-circle" style="margin-top: 76px; left: 350px;"></div>
-            <div class="drop-circle" style="margin-top: 76px; left: 421px;"></div>
-
-        
-            <div class="drop-circle" style="margin-top: 170px; left: 77px;"></div>
-            <div class="drop-circle" style="margin-top: 170px; left: 155px;"></div>
-            <div class="drop-circle" style="margin-top: 170px; left: 233px;"></div>
-            <div class="drop-circle" style="margin-top: 170px; left: 310px;"></div>
-            <div class="drop-circle" style="margin-top: 170px; left: 380px;"></div>
-            
+            <div class="drop-circle" id="circle7" style="margin-top: 76px; left: 43px;"></div>
+            <div class="drop-circle" id="circle8" style="margin-top: 76px; left: 124px;"></div>
+            <div class="drop-circle" id="circle9" style="margin-top: 76px; left: 201px;"></div>
+            <div class="drop-circle" id="circle10" style="margin-top: 76px; left: 280px;"></div>
+            <div class="drop-circle" id="circle11" style="margin-top: 76px; left: 350px;"></div>
+            <div class="drop-circle" id="circle12" style="margin-top: 76px; left: 421px;"></div>
 
 
-            <div class="drop-circle" style="margin-top: 240px; left: 77px;"></div>
-            <div class="drop-circle" style="margin-top: 240px; left: 155px;"></div>
-            <div class="drop-circle" style="margin-top: 240px; left: 233px;"></div>
-            <div class="drop-circle" style="margin-top: 240px; left: 310px;"></div>
-            <div class="drop-circle" style="margin-top: 240px; left: 380px;"></div>
+            <div class="drop-circle" id="circle13" style="margin-top: 170px; left: 77px;"></div>
+            <div class="drop-circle" id="circle14" style="margin-top: 170px; left: 155px;"></div>
+            <div class="drop-circle" id="circle15" style="margin-top: 170px; left: 233px;"></div>
+            <div class="drop-circle" id="circle16" style="margin-top: 170px; left: 310px;"></div>
+            <div class="drop-circle" id="circle17" style="margin-top: 170px; left: 380px;"></div>
+
+
+
+            <div class="drop-circle" id="circle18" style="margin-top: 240px; left: 77px;"></div>
+            <div class="drop-circle" id="circle19" style="margin-top: 240px; left: 155px;"></div>
+            <div class="drop-circle" id="circle20" style="margin-top: 240px; left: 233px;"></div>
+            <div class="drop-circle" id="circle21" style="margin-top: 240px; left: 310px;"></div>
+            <div class="drop-circle" id="circle22" style="margin-top: 240px; left: 380px;"></div>
 
 
             <div class="robotPrimaStem" id="robotPrimaStem" style="margin-top: -165px; margin-left: 430px;">
@@ -243,29 +246,27 @@
     <script>
         const character = document.getElementById("character");
         let queue = [];
+        // const idCircles = document.querySelectorAll("#drop-circle div");
+        
+        const categories = {
+            "mouvement": ["jetonAvancer", "jetonReculer", "jetonAvancerGauche", "jetonAvancerDroite", "jetonMouvementAleatoire"],
+            "rotation": ["jetonRotation", "jetonRepeter2", "jetonRepeter3", "jetonRepeter4", "jetonRepeter5", "jetonRepeter6", "jetonRepeterAleatoire", "jetonRepeter7", "jetonRepeter8", "jetonRepeter9", "jetonRepeter10", "jetonRepeter18"],
+            "nombres": ["jeton1", "jeton2", "jeton3", "jeton4", "jeton5", "jeton6", "jeton7", "jeton8", "jeton9", "jeton10", "jeton15", "jeton20", "jeton36", "jeton40", "jeton45", "jeton50", "jeton60", "jeton72", "jeton90", "jeton100", "jeton108", "jeton120", "jeton135", "jeton144", "jeton150", "jeton180", "jeton360"],
+            "operations": ["jetonPlus1", "jetonPlus2", "jetonPlus3", "jetonPlus5", "jetonPlus10", "jetonPlus15", "jetonPlus20", "jetonPlus50", "jetonPlus100", "jetonMoins1", "jetonMoins2", "jetonMoins3", "jetonMoins5", "jetonMoins10", "jetonMoins15", "jetonMoins20", "jetonMoins50", "jetonMoins100"],
+            "speciaux": ["jetonFonction", "jetonPause", "jetonMultipliePar2", "jetonMultipliePar3", "jetonDivisePar2", "jetonDivisePar3", "jetonPuissanceDe2", "jetonPuissanceDe3", "jetonRacineCarreDeNombreX"]
+        };
 
-        // Fonction pour afficher/masquer les éléments draggables selon le bouton
         function toggleDraggables(category) {
             const allDraggables = document.querySelectorAll(".draggable");
             allDraggables.forEach(item => {
-                item.style.display = "none"; // Masquer tout d'abord
+                item.style.display = "none";
             });
 
-            // Afficher les éléments en fonction de la catégorie sélectionnée
-            const categories = {
-                "mouvement": ["jetonAvancer", "jetonReculer", "jetonAvancerGauche", "jetonAvancerDroite", "jetonMouvementAleatoire"],
-                "rotation": ["jetonRotation", "jetonRepeter2", "jetonRepeter3", "jetonRepeter4", "jetonRepeter5", "jetonRepeter6", "jetonRepeterAleatoire", "jetonRepeter7", "jetonRepeter8", "jetonRepeter9", "jetonRepeter10", "jetonRepeter18"],
-                "nombres": ["jeton1", "jeton2", "jeton3", "jeton4", "jeton5", "jeton6", "jeton7", "jeton8", "jeton9", "jeton10", "jeton15", "jeton20", "jeton36", "jeton40", "jeton45", "jeton50", "jeton60", "jeton72", "jeton90", "jeton100", "jeton108", "jeton120", "jeton135", "jeton144", "jeton150", "jeton180", "jeton360"],
-                "operations": ["jetonPlus1", "jetonPlus2", "jetonPlus3", "jetonPlus5", "jetonPlus10", "jetonPlus15", "jetonPlus20", "jetonPlus50", "jetonPlus100", "jetonMoins1", "jetonMoins2", "jetonMoins3", "jetonMoins5", "jetonMoins10", "jetonMoins15", "jetonMoins20", "jetonMoins50", "jetonMoins100"],
-                "speciaux": ["jetonFonction", "jetonPause", "jetonMultipliePar2", "jetonMultipliePar3", "jetonDivisePar2", "jetonDivisePar3", "jetonPuissanceDe2", "jetonPuissanceDe3", "jetonRacineCarreDeNombreX"]
-            };
-
             categories[category].forEach(id => {
-                document.getElementById(id).style.display = "block"; // Afficher les éléments correspondants
+                document.getElementById(id).style.display = "block";
             });
         }
 
-        // Ajouter des événements aux boutons pour afficher les draggables correspondants
         document.getElementById("mouvement").addEventListener("click", () => toggleDraggables("mouvement"));
         document.getElementById("rotation").addEventListener("click", () => toggleDraggables("rotation"));
         document.getElementById("nombres").addEventListener("click", () => toggleDraggables("nombres"));
@@ -288,16 +289,19 @@
                 event.preventDefault();
                 let action = event.dataTransfer.getData("text");
                 addDraggableElement(action, circle);
+
             });
         });
 
-        function addDraggableElement(action, target) {
+        function addDraggableElement(action, targetElement) {
             let draggedElement = document.createElement("div");
             draggedElement.className = "draggable";
             draggedElement.draggable = true;
-            draggedElement.dataset.action = action;
-            draggedElement.textContent = action;
-            target.appendChild(draggedElement);
+            let img = document.createElement("img");
+            img.setAttribute("src", document.querySelector(".sidebar div").target.id.src);
+            draggedElement.appendChild(img);
+            targetElement.appendChild(draggedElement);
+
 
             draggedElement.addEventListener("dragstart", event => {
                 event.dataTransfer.setData("text", event.target.dataset.action);
@@ -306,220 +310,4 @@
     </script>
 </body>
 
-
-<!-- 
-<div class="grid" id="grid">
-    <div class="supportJeton">
-        <img src="../imagesPlayStem/supportJeton.png" class="imageSupport">
-
-        <div class="nested">
-            <div class="mouvement" id="mouvement"> <button><img src="imagesPlayStem\mouvementCategorie.png"></button></div>
-            <div class="rotation" id="rotation"><button> <img src="imagesPlayStem\repetitionCategorie.png"></button></div>
-            <div class="nombres" id="nombres"><button><img src="imagesPlayStem\nombresCategorie.png"></button></div>
-            <div class="operations" id="operations"><button><img src="imagesPlayStem\operationsCategorie.png"></button></div>
-            <div class="speciaux" id="fonction"><button><img src="imagesPlayStem\speciauxCategorie.png"></button></div>
-        </div>
-
-        <div class="draggableItems">
-            <div class="rangeeJetonsNombres1">
-                <div class="" id="jeton1"> </div>
-                <div class="" id="jeton2"> </div>
-                <div class="" id="jeton3"> </div>
-                <div class="" id="jeton4"> </div>
-                <div class="" id="jeton5"> </div>
-                <div class="" id="jeton6"> </div>
-            </div>
-
-            <div class="rangeeJetonsNombres2">
-
-                <div class="" id="jeton7"> </div>
-                <div class="" id="jeton8"> </div>
-                <div class="" id="jeton9"> </div>
-                <div class="" id="jeton10"> </div>
-                <div class="" id="jeton15"> </div>
-                <div class="" id="jeton20"> </div>
-            </div>
-
-            <div class="rangeeJetonsNombres3">
-
-                <div class="" id="jeton36"> </div>
-                <div class="" id="jeton40"> </div>
-                <div class="" id="jeton45"> </div>
-                <div class="" id="jeton50"> </div>
-                <div class="" id="jeton60"> </div>
-                <div class="" id="jeton72"> </div>
-            </div>
-
-            <div class="rangeeJetonsNombres4">
-
-                <div class="" id="jeton90"> </div>
-                <div class="" id="jeton100"> </div>
-                <div class="" id="jeton108"> </div>
-                <div class="" id="jeton120"> </div>
-                <div class="" id="jeton135"> </div>
-                <div class="" id="jeton144"> </div>
-
-            </div>
-
-            <div class="rangeeJetonsNombres5">
-
-                <div class="" id="jeton150"> </div>
-                <div class="" id="jeton180"> </div>
-                <div class="" id="jeton360"> </div>
-
-            </div>
-
-
-            <div class="rangeeJetonsMouvement1">
-                <div class="" id="jetonAvancer"> </div>
-                <div class="" id="jetonReculer"> </div>
-                <div class="" id="jetonAvancerGauche"> </div>
-                <div class="" id="jetonAvancerDroite"> </div>
-                <div class="" id="jetonMouvementAleatoire"> </div>
-            </div>
-
-
-            <div class="rangeeJetonsRepetition1">
-                <div class="" id="jetonRotation"> </div>
-                <div class="" id="jetonRepeter2"> </div>
-                <div class="" id="jetonRepeter3"> </div>
-                <div class="" id="jetonRepeter4"> </div>
-                <div class="" id="jetonRepeter5"> </div>
-                <div class="" id="jetonRepeter6"> </div>
-            </div>
-
-            <div class="rangeeJetonsRepetition2">
-                <div class="" id="jetonRepeterAleatoire"> </div>
-                <div class="" id="jetonRepeter7"> </div>
-                <div class="" id="jetonRepeter8"> </div>
-                <div class="" id="jetonRepeter9"> </div>
-                <div class="" id="jetonRepeter10"> </div>
-                <div class="" id="jetonRepeter18"> </div>
-            </div>
-
-            <div class="rangeeJetonsRepetition3">
-
-                <div class="" id="jetonRepeter36"> </div>
-                <div class="" id="jetonRepeter72"> </div>
-                <div class="" id="jetonRepeter180"> </div>
-                <div class="" id="jetonRepeter360"> </div>
-            </div>
-
-
-
-            <div class="rangeeJetonsOperations1">
-                <div class="" id="jetonPlus1"> </div>
-                <div class="" id="jetonPlus2"> </div>
-                <div class="" id="jetonPlus3"> </div>
-                <div class="" id="jetonPlus5"> </div>
-                <div class="" id="jetonPlus10"> </div>
-                <div class="" id="jetonPlus15"> </div>
-            </div>
-
-
-            <div class="rangeeJetonsOperations2">
-                <div class="" id="jetonPlus20"> </div>
-                <div class="" id="jetonPlus50"> </div>
-                <div class="" id="jetonPlus100"> </div>
-                <div class="" id="jetonMoins1"> </div>
-                <div class="" id="jetonMoins2"> </div>
-                <div class="" id="jetonMoins3"> </div>
-            </div>
-
-
-            <div class="rangeeJetonsOperations3">
-                <div class="" id="jetonMoins5"> </div>
-                <div class="" id="jetonMoins10"> </div>
-                <div class="" id="jetonMoins15"> </div>
-                <div class="" id="jetonMoins20"> </div>
-                <div class="" id="jetonMoins50"> </div>
-                <div class="" id="jetonMoins100"> </div>
-            </div>
-
-            <div class="rangeeJetonsOperations4">
-                <div class="" id="jetonMultipliePar2"> </div>
-                <div class="" id="jetonMultipliePar3"> </div>
-
-                <div class="" id="jetonDivisePar2"> </div>
-                <div class="" id="jetonDivisePar3"> </div>
-
-                <div class="" id="jetonPuissanceDe2"> </div>
-                <div class="" id="jetonPuissanceDe3"> </div>
-            </div>
-
-            <div class="rangeeJetonsOperations5">
-                <div class="" id="jetonRacineCarreDeNombreX"> </div>
-
-            </div>
-
-            <div class="rangeeJetonsSpeciaux1">
-                <div class="" id="jetonFonction"> </div>
-                <div class="" id="jetonPause"> </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="dropZoneT">
-    <div class="dropZoneT1"></div>
-    <div class="dropZoneT2"></div>
-    <div class="dropZoneT3"></div>
-</div>
-
-<div class="robotPrimaStem" id="robotPrimaStem"><img src="imagesPlayStem\mini-logo.png"></div>
-<div id="grilleBackground" class="imageSupport"> <img src="imagesPlayStem\grilleRobot.png">
-
-</div>
-
-<div id="dropzone" class="telecommande">
-    <img src="../imagesPlayStem/telecommande.png"> -->
-<!-- <div class="dropZoneT"></div>
-    <div class="dropZoneligne1" id="dropZoneligne1">
-        <div class="dropZone1" id="dropZone1"></div>
-        <div class="dropZone2"></div>
-        <div class="dropZone3"></div>
-        <div class="dropZone4"></div>
-        <div class="dropZone5"></div>
-        <div class="dropZone6"></div>
-    </div>
-
-
-    <div class="dropZoneligne2" id="dropZoneligne2">
-        <div class="dropZone7"></div>
-        <div class="dropZone8"></div>
-        <div class="dropZone9"></div>
-        <div class="dropZone10"></div>
-        <div class="dropZone11"></div>
-        <div class="dropZone12"></div>
-
-    </div>
-
-
-
-    <div class="dropZoneligne3" id="dropZoneligne3">
-        <div class="dropZone13"></div>
-        <div class="dropZone14"></div>
-        <div class="dropZone15"></div>
-        <div class="dropZone16"></div>
-        <div class="dropZone17"></div>
-    </div>
-
-
-
-    <div class="dropZoneligne4" id="dropZoneligne4">
-        <div class="dropZone18"></div>
-        <div class="dropZone19"></div>
-        <div class="dropZone20"></div>
-        <div class="dropZone21"></div>
-        <div class="dropZone22"></div>
-    </div> -->
-</div>
-
-
-</div>
-
-
-
-
-</div>
 <?= $this->endSection() ?>
